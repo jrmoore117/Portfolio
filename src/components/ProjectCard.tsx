f3891960781aec194d2af8b96572c462515a33ce
jrmoore117 <jrmoore117@gmail.com>
2023-06-01 15:33:04 -0700

    Use custom hook for placeholder images.

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,34 +1,45 @@
 import Link from 'next/link';
-import Image, { StaticImageData } from 'next/image';
+import Image from 'next/image';
+import usePlaceholder from '@/hooks/usePlaceholder';
 
 interface ProjectCardProps {
   title: string;
   description: string;
   technologies: string[];
   image: {
-    src: StaticImageData;
+    src: string;
     alt: string;
   }
   link: string;
 }
 
-const ProjectCard = ({ title, description, technologies, image, link }: ProjectCardProps) => (
-  <Link href={link} target='blank' className='w-full'>
-    <div className='project-card'>
-      <div className='project-image-wrapper'>
-        <Image src={image.src} alt={image.alt} className='project-image' />
-      </div>
-      <div className='project-info'>
-        <h3 className='project-title'>{title}</h3>
-        <p>{description}</p>
-        <div className='flex flex-wrap'>
-          {technologies && technologies.map((tech) => (
-            <span key={tech} className='project-technology'>{tech}</span>
-          ))}
+const ProjectCard = async ({ title, description, technologies, image, link }: ProjectCardProps) => {
+  const { base64, img } = await usePlaceholder(image.src);
+
+  return (
+    <Link href={link} target='blank' className='w-full'>
+      <div className='project-card'>
+        <div className='project-image-wrapper'>
+          <Image
+            {...img}
+            alt={image.alt}
+            blurDataURL={base64}
+            placeholder="blur"
+            className='project-image'
+          />
         </div>
-      </div>
-    </div> 
-  </Link>
-);
+        <div className='project-info'>
+          <h3 className='project-title'>{title}</h3>
+          <p>{description}</p>
+          <div className='flex flex-wrap'>
+            {technologies && technologies.map((tech) => (
+              <span key={tech} className='project-technology'>{tech}</span>
+            ))}
+          </div>
+        </div>
+      </div> 
+    </Link>
+  );
+}
 
 export default ProjectCard;
